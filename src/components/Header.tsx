1c1fadf6d1ab5c6205d448ffb5b3a2fa0fe2309b
Kim Bum June <bjbj6363@gmail.com>
2018-09-06 11:13:16 +0900

    Updated Header Component Markup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
+import styled, { ThemedBaseStyledInterface } from 'styled-components';
 import * as logo from '../assets/logo';
 
 const HeaderBar = styled.div`
   display: flex;
+  z-index: 10;
+  position: relative;
+  padding-bottom: 2px;
   justify-content: center;
   width: 100%;
   height: 60px;
@@ -16,14 +20,24 @@ const HeaderImage = styled.img`
   padding-right: 7px;
 `;
 
-const HeaderContent = styled.div`
+const HeaderArea = styled.div`
   width: 1366px;
   height: 100%;
   display: flex;
+  align-items: center;
+`;
+
+const HeaderContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
 `;
 
 const MainLogo = styled.div`
   display: flex;
+  position: absolute;
   flex-direction: row;
   align-items: center;
   font-size: 36px;
@@ -31,22 +45,123 @@ const MainLogo = styled.div`
   font-weight: bolder;
 `;
 
-const Menus = styled.div``;
+const MenuContainer = styled.div`
+  width: 800px;
+`;
 
-class Header extends Component {
+interface IMenuProps {
+  name: string;
+  currentSection: string;
+}
+
+const Menu = styled.div`
+  display: inline-block;
+  width: 200px;
+  color: ${({ name, currentSection }: IMenuProps): string =>
+    name === currentSection ? '#1069ef' : '#000000'};
+  text-align: center;
+  font-weight: 500;
+  font-size: 20px;
+`;
+
+const MenuDropDown = styled.div`
+  position: absolute;
+  display: none;
+  z-index: 9;
+  height: 150px;
+  width: 100%;
+  justify-content: center;
+  background-color: #ffffff;
+  border-bottom: 1px solid #cccccc;
+  ${HeaderContent}:hover & {
+    display: flex;
+    top: 63px;
+  }
+`;
+
+const MenuDropDownContentWrapper = styled.div`
+  width: 800px;
+  padding-top: 15px;
+`;
+
+const MenuDropDownItemList = styled.div`
+  display: inline-block;
+  width: 200px;
+  & > a {
+    text-decoration: none;
+    display: inline-block;
+    height: 20px;
+    padding: 5px;
+    width: 190px;
+    color: #000000;
+    &:hover {
+      background-color: #9cbced;
+    }
+  }
+`;
+
+interface IHeaderProps extends RouteComponentProps<any> {
+  // 헤더컴포넌트 로그인데이터 확정나면 넣음될듯
+  // 프레젠테이션? 컨테이너?
+  user?: string;
+}
+
+class Header extends Component<IHeaderProps> {
   public render() {
+    const paths = this.props.location.pathname.split('/');
+    const currentSection: string = paths[1];
+    const currentLocation: string = paths[2];
+    console.log(currentSection);
     return (
       <HeaderBar>
-        <HeaderContent>
+        <HeaderArea>
           <MainLogo>
             <HeaderImage src={logo.logoSquare} />
             THT
           </MainLogo>
-          <Menus>hdi</Menus>
-        </HeaderContent>
+          <HeaderContent>
+            <MenuDropDown>
+              <MenuDropDownContentWrapper>
+                {/* About THT */}
+                <MenuDropDownItemList>
+                  <Link to="/info/introduction">THT</Link>
+                  <Link to="/info/introduction">THT History</Link>
+                  <Link to="/info/introduction">트트인</Link>
+                </MenuDropDownItemList>
+                {/* Events */}
+                <MenuDropDownItemList>
+                  <Link to="/info/introduction">일정공지</Link>
+                  <Link to="/info/introduction">일정공지</Link>
+                  <Link to="/info/introduction">일정공지</Link>
+                </MenuDropDownItemList>
+                {/* Column */}
+                <MenuDropDownItemList />
+                {/* Mentoring */}
+                <MenuDropDownItemList>
+                  <Link to="/info/introduction">일정공지</Link>
+                  <Link to="/info/introduction">일정공지</Link>
+                </MenuDropDownItemList>
+              </MenuDropDownContentWrapper>
+            </MenuDropDown>
+            <MenuContainer>
+              <Menu name="info" currentSection={currentSection}>
+                THT
+              </Menu>
+              <Menu name="board" currentSection={currentSection}>
+                트트 게시판
+              </Menu>
+              <Menu name="column" currentSection={currentSection}>
+                릴레이칼럼
+              </Menu>
+              <Menu name="mento" currentSection={currentSection}>
+                잇다
+              </Menu>
+            </MenuContainer>
+          </HeaderContent>
+        </HeaderArea>
       </HeaderBar>
     );
   }
 }
 
-export default Header;
+export default withRouter(Header);
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router';
 import styled from 'styled-components';
@@ -32,7 +32,7 @@ const App = ({ value, setValue }: IAppProps) => (
       {value}
       <button onClick={() => setValue(value + 1)}>Up</button>
       <button onClick={() => setValue(value - 1)}>Down</button>
-      <Route exact path="/info" component={Introduction} />
+      <Route exact path="/info/introduction" component={Introduction} />
       <Route exact path="/info/history" />
     </MainContainer>
   </AppBody>
@@ -45,6 +45,7 @@ const mapStateToProps = (state: IRootState) => ({
 // 2번째 인자로 object를 넘겨주면, 각 키의 해당하는 값이 actionCreator임을 알고 dispatch한다.
 // typescript에서 connect 정의하는 여러 방법이 있는데,
 // https://github.com/piotrwitek/react-redux-typescript-guide 참고
+
 export default connect(
   mapStateToProps,
   {
