efb0ff894accff6b574cd767961a4c6958fd32ed
Kim Bum June <bjbj6363@gmail.com>
2019-02-13 18:09:30 +0900

    아이콘 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+const passIcon = require('./pass.png');
+const failIcon = require('./fail.png');
+
+export { passIcon, failIcon };
--- src/components/CheckInput.tsx
+++ src/components/CheckInput.tsx
@@ -1,6 +1,39 @@
 import React, { InputHTMLAttributes, ReactChild } from 'react';
 import styled from 'styled-components';
+import { passIcon, failIcon } from '../assets/icons';
 import SignInput from './SignInput';
+import colors from '../constants/colors';
+
+const InfoContent = styled.div<{ condition: boolean }>`
+  color: ${({ condition }) => (condition ? colors.positive : colors.negative)};
+  font-size: 16px;
+  margin-top: 4px;
+  margin-left: 6px;
+  line-height: 20px;
+`;
+
+const InfoIcon = styled.img`
+  vertical-align: bottom;
+  width: 18px;
+  height: 18px;
+  padding: 2px;
+`;
+
+interface IInfoText {
+  condition: boolean;
+  children: ReactChild;
+}
+
+const InfoText = ({ condition, children }: IInfoText) => (
+  <InfoContent condition={condition}>
+    <InfoIcon src={condition ? passIcon : failIcon} />
+    {children}
+  </InfoContent>
+);
+
+const CheckInputWrapper = styled.div`
+  margin-bottom: 16px;
+`;
 
 const InputWrapper = styled.div`
   display: flex;
@@ -25,13 +58,17 @@ const CheckInput = ({
   rightComponent = null,
 }: ICheckInputProps) => {
   return (
-    <div>
+    <CheckInputWrapper>
       <InputWrapper>
         <SignInput placeholder={placeholder} value={value} onChange={onChange} type={type} />
         {rightComponent}
       </InputWrapper>
-      {value && typeof value === 'string' && value.length && isValid ? validInfo : inValidInfo}
-    </div>
+      {value && typeof value === 'string' && value.length && isValid ? (
+        <InfoText condition={isValid}>{validInfo}</InfoText>
+      ) : (
+        <InfoText condition={isValid}>{inValidInfo}</InfoText>
+      )}
+    </CheckInputWrapper>
   );
 };
 
