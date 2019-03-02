c24decc656eb363b1434531ce7dc3b6728dc74ed
Kim Bum June <bjbj6363@gmail.com>
2019-03-03 02:58:26 +0900

    refactor: 쓸모없는 파일 삭제

--- src/containers/SignUp/SecondStep.tsx
+++ src/containers/SignUp/SecondStep.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import useFormState from '../../hooks/useFormState';
 import { ISignupForm } from '../../pages/SignUp';
 import { InputFooter, InputWrapper, StepButton, UserInfoInput } from '../../styles/SingUpStyles';
-import { UserInfoFormMap } from './SignUpFormMap';
 
 type SecondFormType = Pick<ISignupForm, 'name' | 'phoneNumber' | 'major' | 'studentId' | 'period'> & {
   [key: string]: string;
@@ -12,6 +11,18 @@ interface ISecondStepProps {
   getForm: () => ISignupForm;
   setStep: ({ nextStep, nextForm }: { nextStep: number; nextForm: SecondFormType }) => void;
 }
+
+/**
+ * UserInfo 에 대한 input 설정
+ */
+const UserInfoFormMap: { [key: string]: InputHTMLAttributes<{}> } = {
+  name: { placeholder: '이름', type: 'text' },
+  phoneNumber: { placeholder: '전화번호', type: 'tel' },
+  major: { placeholder: '전공', type: 'text' },
+  studentId: { placeholder: '학번', type: 'number', min: '00', max: '99' },
+  period: { placeholder: '활동시기', type: 'month', min: '2018-03' },
+};
+
 const SecondStep: React.SFC<ISecondStepProps> = ({ getForm, setStep }) => {
   const { username, password, pwCheck, ...rest } = getForm();
 
--- src/containers/SignUp/SignUpFormMap.ts
+++
@@ -1,9 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-
-export const UserInfoFormMap: { [key: string]: InputHTMLAttributes<{}> } = {
-  name: { placeholder: '이름', type: 'text' },
-  phoneNumber: { placeholder: '전화번호', type: 'tel' },
-  major: { placeholder: '전공', type: 'text' },
-  studentId: { placeholder: '학번', type: 'number', min: '00', max: '99' },
-  period: { placeholder: '활동시기', type: 'month', min: '2018-03' },
-};
